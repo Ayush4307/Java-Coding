d3777f7b73400c277d66064547ddaea5c0cdaa50
Ayush4307 <singhayush4307@gmail.com>
2026-05-29 21:38:30 +0530

    Improve ArrayReverse: Add Javadoc to reverseArray method detailing complexity

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -1,4 +1,13 @@
+/**
+ * ArrayReverse contains utility methods for reversing arrays in-place.
+ */
 public class ArrayReverse {
+    /**
+     * Reverses an array of integers in-place.
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     * @param arr The array to be reversed
+     */
     public static void reverseArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
