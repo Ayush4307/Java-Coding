76ce08c369673aa7c54bfee940b8a24438c38064
Ayush4307 <singhayush4307@gmail.com>
2026-05-29 21:37:57 +0530

    Add ReverseLinkedList.java: Iterative reversal of singly linked list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,40 @@
+//AYUSH SINGH PAWAR
+class Node {
+    int data;
+    Node next;
+}
+public class ReverseLinkedList {
+    static Node reverse(Node head) {
+        Node newHead = null;
+        while (head != null) {
+            Node temp = head;
+            head = head.next;
+            temp.next = newHead;
+            newHead = temp;
+        }
+        return newHead;
+    }
+    static void print(Node head) {
+        Node t = head;
+        while (t != null) {
+            System.out.print(t.data + " ");
+            t = t.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head = new Node();
+        head.data = 1;
+        head.next = new Node();
+        head.next.data = 2;
+        head.next.next = new Node();
+        head.next.next.data = 3;
+        head.next.next.next = new Node();
+        head.next.next.next.data = 4;
+        System.out.println("Original List:");
+        print(head);
+        head = reverse(head);
+        System.out.println("Reversed List:");
+        print(head);
+    }
+}
