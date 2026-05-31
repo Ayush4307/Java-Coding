baa72c53a07c31cba52edeb4947a3a2f0941dbeb
Ayush4307 <singhayush4307@gmail.com>
2026-05-31 20:46:53 +0530

    feat: add Insertion Sort implementation

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void sort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 90, 3};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("Sorted Array:   " + Arrays.toString(arr));
+    }
+}
