efbed5ea7c26376d72f5afaf0c2f9381ee3cb083
Ayush4307 <singhayush4307@gmail.com>
2026-05-31 20:46:21 +0530

    feat: add Selection Sort implementation

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = array[minIdx];
+            array[minIdx] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11, 90, 11};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("Sorted Array:   " + Arrays.toString(arr));
+    }
+}
