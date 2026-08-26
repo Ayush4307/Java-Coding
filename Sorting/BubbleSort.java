6a1acf4ba74252ac9079905f3de806f4480f8298
Ayush4307 <singhayush4307@gmail.com>
2026-08-26 21:08:19 +0530

    Skip sorted tail in Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -11,18 +11,21 @@ public class BubbleSort {
      */
     public static void sort(int[] array) {
         if (array == null || array.length <= 1) return;
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
+        int lastUnsorted = array.length - 1;
+        while (lastUnsorted > 0) {
             boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
+            int lastSwap = 0;
+            for (int j = 0; j < lastUnsorted; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                     swapped = true;
+                    lastSwap = j + 1;
                 }
             }
             if (!swapped) break;
+            lastUnsorted = lastSwap;
         }
     }
 
