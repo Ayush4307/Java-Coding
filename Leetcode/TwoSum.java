fd388fa9dc7a4dec27650f683677a427a5019b3d
Ayush4307 <singhayush4307@gmail.com>
2026-08-26 21:07:36 +0530

    Optimize Two Sum with a lookup map

--- Leetcode/TwoSum.java
+++ Leetcode/TwoSum.java
@@ -1,11 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[] { seen.get(complement), i };
             }
+            seen.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
